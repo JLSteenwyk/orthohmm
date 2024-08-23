ff65c7c7d0eac9c5c6696368580383c0ff18c360
JLSteenwyk <jlsteenwyk@gmail.com>
2024-08-23 14:32:34 -0700

    fixed parser check for specified substitution matrix

--- orthohmm/parser.py
+++ orthohmm/parser.py
@@ -10,6 +10,7 @@ from argparse import (
 from .helpers import (
     StartStep,
     StopStep,
+    SubstitutionMatrix,
 )
 from .version import __version__
 
@@ -195,7 +196,7 @@ def create_parser() -> ArgumentParser:
         metavar="output_directory"
     )
 
-    substitution_matrix_choices = [step.value for step in StartStep]
+    substitution_matrix_choices = [step.value for step in SubstitutionMatrix]
     optional.add_argument(
         "-x",
         "--substitution_matrix",
